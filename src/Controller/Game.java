403b1e11f3deb73af16dcbc1d388d4bc187f95b7
Fareed Ahmad <fareed.ah478@gmail.com>
2018-10-02 14:19:45 -0400

    Updated view

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -1,3 +1,5 @@
+package Controller;
+
 public class Game{
     public static void main(String[] args){
 
--- src/View/Backyard.java
+++ src/View/Backyard.java
@@ -1,8 +1,44 @@
-public class Backyard{
+package View;
 
+import Model.Sprite;
 
+public class Backyard {
 
+    private String map[][];
+    private static final int HEIGHT = 5;
+    private static final int WIDTH = 18;
 
+    public Backyard() {
+        map = new String[HEIGHT][WIDTH];
+    }
 
+    /**
+     * Adds a new sprite to the map
+     *
+     * @param x      x coordinate for the plant
+     * @param y      y coordinate for the plant
+     * @param sprite Which type of plant is being added
+     */
+    public void addSprite(int x, int y, Sprite sprite) {
+        map[x][y] = sprite.getName();
+    }
 
+    /**
+     * Removes a sprite from the map
+     *
+     * @param x x coordinate of plant to remove
+     * @param y y coordinate of plant to remove
+     */
+    public void removeSprite(int x, int y) {
+        map[x][y] = null;
+    }
+
+    public void print() {
+        for (int row = 0; row < HEIGHT; row++) {
+            for(int col = 0; col <WIDTH;col++){
+                System.out.print(map[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
 }

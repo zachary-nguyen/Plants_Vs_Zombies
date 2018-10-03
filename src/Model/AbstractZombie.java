d9d3d96239408c54feec7466889fda5cb072c9db
ericcosoreanu <eric.cosoreanu@gmail.com>
2018-10-03 11:52:18 -0400

    Added AbstractZombie & Zombie. Fixed Game.

--- src/Model/AbstractZombie.java
+++ src/Model/AbstractZombie.java
@@ -5,4 +5,15 @@ package Model;
  */
 public abstract class AbstractZombie extends Sprite{
 
+    private int speed;
+
+    public AbstractZombie() {
+        super();
+        this.speed = 10;
+    }
+
+    public AbstractZombie(String name, int health, int damage, int speed) {
+        super(name, health, damage);
+        this.speed = speed;
+    }
 }

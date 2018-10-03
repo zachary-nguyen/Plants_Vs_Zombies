a1d003408af23e1771df5cc99f28cf64ada0044f
zacharynguyen <zack.nguyen@hotmail.com>
2018-10-03 12:31:26 -0400

    - Add helper method for main

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -7,58 +7,74 @@ import java.util.Scanner;
 
 public class Game{
 
-    private int score;
-
-    private int money;
-
+    private Backyard backyard;
     /**
      * Constructor for game.
      */
     public Game(){
-        this.score = 0;
-        this.money = 250;
+        this.backyard = new Backyard();
+    }
+
+    /**
+     * Helper method for main method to add sprite.
+     */
+    private void addSprite(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select coordinates in format 'X Y' ");
+
+        String response = scanner.nextLine();
+
+        while(true) {
+
+            String[] coordinate = response.trim().split(" ");
+
+            if (coordinate.length == 2) {
+                int xValueToAdd = Integer.valueOf(coordinate[0]);
+                int yValueToAdd = Integer.valueOf(coordinate[1]);
+
+                //Make sure coordinates are in bound
+                if (xValueToAdd >= 0 && xValueToAdd < 19 && yValueToAdd >= 0 && yValueToAdd < 5) {
+                    backyard.addSprite(xValueToAdd, yValueToAdd, new Peashooter());
+                    break;
+                } else {
+                    System.out.println("Coordinates out of bonds!");
+                }
+            } else {
+                System.out.println("Invalid coordinates!");
+            }
+            System.out.println("Select coordinates in format 'X Y' ");
+
+        }
     }
 
     public static void main(String[] args){
 
-        //Set up the game
-        final Game game = new Game();
-        final Backyard backyard = new Backyard();
+            //Set up the game
+            final Game game = new Game();
 
-        //user input
-        Scanner scanner = new Scanner(System.in);
+            //user input
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Type anything to start game :");
-        String response = scanner.next();
+            System.out.println("Type anything to start game :");
+            String response = scanner.next();
 
-        while(!response.equals("exit")){
-            backyard.print();
+            while (!response.equals("exit")) {
+                //Print the backyard
+                game.getBackyard().print();
 
-            System.out.println("What is your move? 'Add' 'Remove' 'Skip' ");
-            response = scanner.next();
-            response = response.trim().toLowerCase();
-            scanner.nextLine();
+                System.out.println("What is your move? 'Add' 'Remove' 'Skip' 'Exit'");
+                response = scanner.next();
+                response = response.trim().toLowerCase();
+                scanner.nextLine();
 
-            if(response.equals("add")){
-                boolean addFlag = false;
-                while(true) {
-
-                    System.out.println("Select coordinates in format 'X Y' ");
-                    response = scanner.nextLine();
-
-                    String[] coordinate = response.trim().split(" ");
-                    int xValueToAdd = Integer.valueOf(coordinate[0]);
-                    int yValueToAdd = Integer.valueOf(coordinate[1]);
-
-                    //Make sure coordinates are in bound
-                    if (xValueToAdd >= 0 && xValueToAdd < 19 && yValueToAdd >= 0 && yValueToAdd < 5) {
-                        backyard.addSprite(xValueToAdd, yValueToAdd, new Peashooter());
-                        break;
-                    } else {
-                        System.out.println("Wrong coordinates!");
-                    }
+                //Treat User response
+                if (response.equals("add")) {
+                    game.addSprite();
                 }
             }
-        }
+    }
+
+    public Backyard getBackyard() {
+        return backyard;
     }
 }

d017e49d6281d87c3235373330c6213a11ae3d8d
Mathew <mathew2008@rogers.com>
2018-11-25 17:59:49 -0500

    fixed bugs

--- src/Model/Backyard.java
+++ src/Model/Backyard.java
@@ -140,19 +140,18 @@ public class Backyard implements Cloneable {
                         if (col - zombie.getSpeed() <= -1) { //end game if zombie reaches the end
                             Game.gameOver = true;
                         } else {
-                            for (int i = 1; i <=zombie.getSpeed(); i++) {
-                                for (Sprite nextPlant : map[row][col - i]) {
-                                    // for each sprite in the queue
-                                    if (nextPlant instanceof AbstractPlant) {
-                                        moveZombie = false;
-                                        AbstractPlant plant = (AbstractPlant) nextPlant;
-                                        plant.setHealth(plant.getHealth() - zombie.getDamage());
-                                        if (plant.getHealth() < 0) {
-                                            removePlant = plant;
-                                        }
+                            for (Sprite nextPlant : map[row][col - zombie.getSpeed()]) {
+                                // for each sprite in the queue
+                                if (nextPlant instanceof AbstractPlant) {
+                                    moveZombie = false;
+                                    AbstractPlant plant = (AbstractPlant) nextPlant;
+                                    plant.setHealth(plant.getHealth() - zombie.getDamage());
+                                    if (plant.getHealth() < 0) {
+                                        removePlant = plant;
                                     }
                                 }
                             }
+
                             if (moveZombie) {
                                 map[row][col - zombie.getSpeed()].add(zombie);
                                 iter.remove();
@@ -204,7 +203,7 @@ public class Backyard implements Cloneable {
                             }
                         }
                     }
-                }
+            }
                 map[row][col].remove(deadZombie);
             }
         }
